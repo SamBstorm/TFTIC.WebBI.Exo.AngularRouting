da1a24624c5c65a79d1f6d09b68a375b5eab6875
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-13 14:22:50 +0200

    Update create-fan

--- src/app/components/fan-create/fan-create.component.ts
+++ src/app/components/fan-create/fan-create.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { Fan } from './../../models/fan';
 import { FanMockupService } from './../../services/fan-mockup.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -13,13 +13,18 @@ export class FanCreateComponent implements OnInit {
 
   public createForm! : FormGroup;
 
+  public get createFormSeriesArray() : FormArray{
+    return this.createForm.controls['series'] as FormArray;
+  } 
+
   constructor(private _formBuilder : FormBuilder, private _service : FanMockupService, private _router : Router) { }
 
   ngOnInit(): void {
     this.createForm = this._formBuilder.group({
       firstname : [null,Validators.required],
       lastname : [null,Validators.required],
-      birthdate : [null,Validators.required]
+      birthdate : [null,Validators.required],
+      series : this._formBuilder.array([])
     });
   }
 
@@ -30,8 +35,13 @@ export class FanCreateComponent implements OnInit {
         this.createForm.value['firstname'],
         this.createForm.value['lastname'],
         this.createForm.value['birthdate'],
+        ...this.createFormSeriesArray.value
         );
       this._router.navigateByUrl('details/'+this._service.Insert(fan));
     }
+  }
+
+  public addSerieInput(){
+    this.createFormSeriesArray.push(this._formBuilder.control(null, Validators.required));
   }
 }
